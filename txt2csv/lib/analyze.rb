60931bede1ba24cd07c06a0ff002df639a89e1c3
hollocherDale <hollocher@gmail.com>
2014-02-11 17:54:17 -0800

    didnt merge, just c&ped

--- txt2csv/lib/analyze.rb
+++ txt2csv/lib/analyze.rb
@@ -1,6 +1,8 @@
 require "pry"
-#Add case statement to check to see if you are looking for prefixes or suffixes
+#create Hash to store patterns and counts - create with default
+histogram_hash = Hash.new(0)
 
+#Add case statement to check to see if you are looking for prefixes or suffixes
 case ARGV[0]
   when '-p'
     # set up some regular expression for prefixes
@@ -16,22 +18,11 @@ end
 
 ARGV.shift
 
-#create Hash to store patterns and counts - create with default
-h = Hash.new(0)
-
-#Open up file, read through it checking for regex pattern on each line
-	ARGF.readlines.each do |line|
-		m = pattern.match(line)
-		if m
-			h[m[0]] += 1
-		end
-	end
-
+ARGF.readlines.each { |line| histogram_hash[pattern.match(line)[0]] += 1 }
 
 #sort hash from largest to smallest value
-h = h.sort_by { |a, b| b}
-h.reverse!
+histogram_hash = histogram_hash.sort_by { |a, b| b}
+histogram_hash.reverse!
 
 #print hash to a new file called histogram with each key and value on separate line
-h.each { |k,v| puts "#{k} #{v}" }
-
+histogram_hash.each { |k,v| puts "#{k} #{v}" }
